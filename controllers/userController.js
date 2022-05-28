6f3a6281e9bf1a6a61dd1c3d00cc354643ff8f1a
Talha Zulfiqar <csharpjava0@gmail.com>
2022-05-29 01:55:17 +0500

    complete the jwt authentication , send the onlyhttp cookies

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,9 +11,6 @@ const generateToken = (id) => {
   });
 };
 
-// @desc    Register new user
-// @route   POST /api/users
-// @access  Public
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
 
@@ -55,9 +52,6 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
-// @desc    Authenticate a user
-// @route   POST /api/users/login
-// @access  Public
 const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
@@ -86,9 +80,6 @@ const loginUser = asyncHandler(async (req, res) => {
   }
 });
 
-// @desc    Get user data
-// @route   GET /api/users/me
-// @access  Private
 const getMe = asyncHandler(async (req, res) => {
   res.status(200).json(req.user);
 });
